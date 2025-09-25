5377bc00cde40743b4e3a3748925c5fdb26ff4f0
fluidstar1338 <joao12rt@gmail.com>
2025-09-25 19:46:04 -0300

    continuacao

--- src/academy/devdojo/maratonajava/Aula06EstruturasDeRepeticao04.java
+++ src/academy/devdojo/maratonajava/Aula06EstruturasDeRepeticao04.java
@@ -2,7 +2,22 @@ package academy.devdojo.maratonajava;
 
 public class Aula06EstruturasDeRepeticao04 {
     public static void main(String[] args) {
+        //dado o valor de um carro descubra quantas vezes ele pode ser parcelado
+        //condicao é que o valor da parcela precisa ser >= 1000
 
+        double valorTotal = 200000;
+        double parcelas;
+        double qtdParcelas;
+        int x = 0;
+
+        do {
+            parcelas = valorTotal / x;
+            x++;
+        }while (parcelas > 1000);
+
+        qtdParcelas = valorTotal / parcelas;
+        System.out.println("Valor das parcelas: "+ parcelas);
+        System.out.println("Quantidade de parcelas: "+ qtdParcelas);
 
 
     }
